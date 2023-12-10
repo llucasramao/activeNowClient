071a86d7ad1dbec5a97e3e58ad7af551729a6f01
Lucas Ramão <ext-lanascimento@bbts.com.br>
2023-12-09 23:29:05 -0300

    fix: cron fix

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"activeNow/functions"
 	logger "activeNow/log"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
 	"runtime"
 	"strings"
 	"time"
@@ -18,7 +20,7 @@ const manager = "http://192.168.1.14:7654"
 func main() {
 	if runtime.GOOS == "linux" {
 		Cron("5s")
-		//listen()
+		listen()
 	} else {
 		logger.Log("Esse agente so pode ser utilizado em sistema linux!", true)
 		return
@@ -35,9 +37,18 @@ func Cron(sched string) {
 	c.Start()
 }
 
+func listen() {
+	// Função cirada para manter o cron ininterrupto
+	sig := make(chan os.Signal)
+	signal.Notify(sig, os.Interrupt, os.Kill)
+	<-sig
+	fmt.Println(time.Now().String() + " - Closed")
+}
+
 func Init() {
 	if isUpdate() {
-		functions.Finder()
+		//functions.Finder()
+		fmt.Println(1)
 	} else {
 		time.Sleep(time.Second * 3)
 		logger.Log("Tentando auto atualizar", false)
