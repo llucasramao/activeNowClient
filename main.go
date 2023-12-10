c40a1c488c6feb68db3d761b462cb940be565879
Lucas Ramão <ext-lanascimento@bbts.com.br>
2023-12-10 00:07:04 -0300

    fix: GOOS

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ const version = "0.3.5"
 const manager = "http://192.168.1.14:7654"
 
 func main() {
-	if runtime.GOOS != "linux" {
+	if runtime.GOOS == "linux" {
 		Cron("5s")
 		listen()
 	} else {
