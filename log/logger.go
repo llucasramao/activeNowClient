67d70a845df3358c80b0de9ac44d0d16b2776262
Lucas Ramão <ext-lanascimento@bbts.com.br>
2023-12-12 16:13:48 -0300

    feat: enable logging

--- log/logger.go
+++ log/logger.go
@@ -2,21 +2,24 @@ package logger
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/exec"
 )
 
 func Log(message string, isError bool) {
-	// logDir := exec.Command("mkdir", "-p", "/var/log/activeNow")
-	// _, err := logDir.Output()
-	// if err != nil {
-	// 	Log(err.Error(), true)
-	// 	return
-	// }
-	// file, err := os.OpenFile("/var/log/activeNow/history.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer file.Close()
-	// logger := log.New(file, "", log.LstdFlags)
+	logDir := exec.Command("mkdir", "-p", "/var/log/activeNow")
+	_, err := logDir.Output()
+	if err != nil {
+		Log(err.Error(), true)
+		return
+	}
+	file, err := os.OpenFile("/var/log/activeNow/history.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	logger := log.New(file, "", log.LstdFlags)
 
 	if isError {
 		message = "[-] ERROR: " + message
@@ -24,6 +27,6 @@ func Log(message string, isError bool) {
 		message = "[+] INFO: " + message
 	}
 
-	//logger.Println(message)
+	logger.Println(message)
 	fmt.Println(message)
 }
