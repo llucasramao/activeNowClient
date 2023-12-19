ccb5b3fe0b511bc4acd7782d3dba176d0d47d415
Lucas Ramão <ext-lanascimento@bbts.com.br>
2023-12-18 21:05:13 -0300

    fix: cron time

--- config/config.go
+++ config/config.go
@@ -1,5 +1,5 @@
 package config
 
-var CronTime = "10s-test"
-var AgentVersion = "0.3.11-" + CronTime
+var CronTime = "10s"
+var AgentVersion = "0.3.11-" + CronTime + "-test"
 var Manager = "http://192.168.1.14:7654"
